9f096df3e17dd100070497eb4f04ca5356802945
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-19 14:25:35 -0600

    basic game with button listeners

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentHumanChoice = '';
+let currentComputerChoice = '';
 const div = document.querySelector("div");
-div.addEventListener("click", onButtonClick);
+const score = document.querySelector("#score");
+const currentChoices = document.querySelector("#current-choices")
 
 const onButtonClick = function(e) {
-    
+    switch (e.target.id) {
+        case 'rock':
+            currentHumanChoice = 'rock';
+            break;
+        case 'paper':
+            currentHumanChoice = 'paper';
+            break;
+        case 'scissors':
+            currentHumanChoice = 'scissors';
+            break;
+    }
+    playGame();
 }
+
+div.addEventListener("click", onButtonClick);
 
 function getComputerChoice () {
     let num = Math.floor(Math.random() * 3);
@@ -37,52 +52,53 @@ function getComputerChoice () {
 */
 
 function playRound() {
-    let humanChoice = getHumanChoice();
+    //let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
+    currentComputerChoice = computerChoice;
 
-    if (humanChoice == "rock") {
+    if (currentHumanChoice == "rock") {
         if (computerChoice == "scissors" ) {
             humanScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("You win the round!");
             return 
         } else if (computerChoice == "paper") {
             computerScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("Computer wins the round!");
             return;
         } else if (computerChoice == "rock") {
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("A tie!");
         }
-    } else if (humanChoice == "scissors") {
+    } else if (currentHumanChoice == "scissors") {
         if (computerChoice == "paper" ) {
             humanScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("You win the round!");
             return 
         } else if (computerChoice == "rock") {
             computerScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("Computer wins the round!");
             return;
         } else if (computerChoice == "scissors") {
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("A tie!");
         }
-    } else if (humanChoice == "paper") {
+    } else if (currentHumanChoice == "paper") {
         if (computerChoice == "rock" ) {
             humanScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("You win the round!");
             return 
         } else if (computerChoice == "scissors") {
             computerScore += 1;
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("Computer wins the round!");
             return;
         } else if (computerChoice == "paper") {
-            console.log(`You picked ${humanChoice} the computer picked ${computerChoice}`);
+            console.log(`You picked ${currentHumanChoice} the computer picked ${computerChoice}`);
             console.log("A tie!");
         }
     }
@@ -93,7 +109,10 @@ function playGame() {
     //while (roundCount < 5) {
     playRound()
     console.log(`Score:\nYou: ${humanScore} Computer: ${computerScore}`);
+    score.textContent = `Human: ${humanScore}  Computer: ${computerScore}`
+    currentChoices.textContent = `You picked ${currentHumanChoice} the computer picked ${currentComputerChoice}`;
     roundCount += 1;
+    console.log("hello")
     //}
     /*
     if (humanScore > computerScore) {
@@ -106,4 +125,4 @@ function playGame() {
     */
 }
 
-playGame();
+//playGame();
